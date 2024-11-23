34f847d447a0a920b1cfb6496c6078369b1ea7cf
alokkv2502 <alok930528@gmail.com>
2024-11-23 10:54:35 +0530

    inital

--- converter/settings.py
+++ converter/settings.py
@@ -127,7 +127,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'documents/static']
 
-
+if os.getenv('VERCEL'):
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
